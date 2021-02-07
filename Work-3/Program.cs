2971cd103d26c727b3c2b0a3c828d452f28245cd
Emre Bilal <emre_bilal21@hotmail.com>
2021-02-07 23:43:41 +0300

    Generics, constructor

---
+++ Work-3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Work_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> countries = new MyList<string>();
+            countries.Add("Turkey");
+            countries.Add("England");
+            countries.Add("Germany");
+
+            countries.View();
+        }
+    }
+}
