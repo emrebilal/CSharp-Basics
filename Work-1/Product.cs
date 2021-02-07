ec7907760904fa95d9129013d88d9f00157a3820
Emre Bilal <emre_bilal21@hotmail.com>
2021-02-07 23:40:38 +0300

    Class, method, encapsulation

---
+++ Work-1/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Work_1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int StockNumber { get; set; }
+    }
+}
---
+++ Work-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "ASUS Laptop";
+            product1.Price = 1500;
+            product1.Description = "Gamebook";
+
+            Product product2 = new Product();
+            product2.Name = "MSI Mouse";
+            product2.Price = 250;
+            product2.Description = "Game Mouse";
+
+            Product[] products = new Product[]{product1,product2};
+
+            foreach(var product in products){
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Description + "\n");
+            }
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Remove(product1);
+        }
+    }
+}
